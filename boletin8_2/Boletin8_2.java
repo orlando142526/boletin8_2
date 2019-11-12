2f41d415c6171f3e912ea532bb72361e28db6abb
orlando123-2 <Viper1999>
2019-11-12 10:43:31 +0100

    first commit

---
+++ boletin8_2/Boletin8_2.java
@@ -0,0 +1,16 @@
+
+package boletin8_2;
+
+public class Boletin8_2 {
+
+   
+    public static void main(String[] args) {
+        Calculo obx = new Calculo();
+obx.calcular();
+        
+        
+        
+        
+    }
+    
+}
